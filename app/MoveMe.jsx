f033072e4af62c6f5e1c18383da21f178de69441
06AlexS2 <72035459+06AlexS2@users.noreply.github.com>
2023-05-26 17:45:51 -0600

    Update MoveMe.jsx

--- app/MoveMe.jsx
+++ app/MoveMe.jsx
@@ -7,33 +7,33 @@ import MoveableChild from "./MoveableChild";
 export default function MoveMe() {
   //set the url for the image
   const IMG_URL = "https://via.placeholder.com/200x200.png?text=Move+me!";
-  //with useState hook, we set the variables for position, size and rotation in the image
+  //with useState hook, I set the variables for position, size and rotation in the image
   //both position and size are objects that contain the properties needed for their context
   const [imgPos, setImgPos] = useState({ posX: 0, posY: 0 });
   let [imgSize, setImgSize] = useState({ width: 200, height: 200 });
   const [imgRotation, setImgRotation] = useState(0);
 
-  //with handlePosition, we modify the parameters of the image rendering in the page
-  //we need to extract the numbers from the string parameters of the transform style, so we use regex
+  //with handlePosition, I modified the parameters of the image rendering in the page
+  //I needed to extract the numbers from the string parameters of the transform style, so I used regex
   const handlePosition = (e) => {
     const regex = /-?\d+/g;
-    //we force the value to match the regex
+    //I forced the value to match the regex
     const numbersArray = e.transform.match(regex);
-    //now, with the react hook, we set the properties
+    //now, with the react hook, I set the properties
     setImgPos({
       posX: numbersArray[0] ? parseInt(numbersArray[0]) : 0,
       posY: numbersArray[1] ? parseInt(numbersArray[1]) : 0,
     });
-    //finally, using string literals, we modify the style parameters with the new position values
+    //finally, using string literals, I modify the style parameters with the new position values
     e.target.style.transform = `translate(${imgPos.posX}px, ${imgPos.posY}px)`;
   };
-  //same for the rotation handler as the position, we need to extract only the digits in the "rotate" style parameter
+  //same for the rotation handler as the position, it needs to extract only the digits in the "rotate" style parameter
   const handleRotation = (e) => {
     const regex = /rotate\((-?\d+(?:\.\d+)?)deg\)/;
 
     const match = e.transform.match(regex);
     setImgRotation(parseFloat(match[1]));
-    //in this case, we need not only to modify the rotation, but the translate as well, because if we don't, the values
+    //in this case, I need not only to modify the rotation, but the translate as well, because if I don't, the values
     //will restart
     e.target.style.transform = `translate(${imgPos.posX}px, ${imgPos.posY}px) rotate(${imgRotation}deg)`;
   };
@@ -45,7 +45,7 @@ export default function MoveMe() {
     target.style.height = `${height}px`;
     setImgSize({ width, height });
   };
-  //here, we render the page, and since we're passing props to a child component, this is the final step in this script.
+  //here, I render the page, and since I'm passing props to a child component, this is the final step in this script.
   return (
     <div
       className="target z-30"
